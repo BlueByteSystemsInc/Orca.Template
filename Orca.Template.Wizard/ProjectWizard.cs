e9eb793a6a02d199563334feae83893d1254f515
Andrey Arekhva <a.arekhva@gmail.com>
2022-01-21 17:49:31 +0300

    #1 Add projects for VS template

---
+++ Orca.Template.Wizard/ProjectWizard.cs
@@ -0,0 +1,40 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.TemplateWizard;
+using System;
+using System.Collections.Generic;
+
+namespace Orca.Template.Wizard
+{
+    public class ProjectWizard : IWizard
+    {
+        public void BeforeOpeningFile(ProjectItem projectItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProjectFinishedGenerating(Project project)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunFinished()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ShouldAddProjectItem(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Orca.Template/PDMTest.cs
@@ -0,0 +1,89 @@
+﻿/*By using Orca and this template, you acknowledge and agree to all the terms of usage and conditions specified at this link
+https://raw.githubusercontent.com/BlueByteSystemsInc/Orca.Docs/main/LICENSE.md
+If the terms of usage and conditions are not accessible, please request a copy from amen@bluebyte.biz or by contacting us from www.bluebyte.biz
+*/
+using BlueByte.SOLIDWORKS.PDMProfessional.UnitTesting;
+using BlueByte.SOLIDWORKS.PDMProfessional.UnitTesting.Attributes;
+using EPDM.Interop.epdm;
+using System;
+
+namespace $safeprojectname$
+{
+    public static class LicenseInformation
+    {
+        public static string licensePath = "Path to XML license file";
+    }
+
+    #region startup
+    class Host
+    {
+
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string licenseKey = string.Empty;
+
+
+            licenseKey = System.IO.File.ReadAllText(LicenseInformation.licensePath);
+
+            var instance = new PDMTest(licenseKey);
+
+            instance.Run();
+            Console.ReadLine();
+        }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Todo: 
+    /// change T to your a class that implements <see cref="IEdmAddIn5"/>
+    /// change TestVault to use your testing vault
+    /// </summary>
+    [TestVault(Name = "bluebyte")]
+    public class PDMTest : TestableAddInBase<T>
+    {
+        #region Constructor
+        public PDMTest(string licenseKey) : base(licenseKey)
+        {
+
+        }
+
+        #endregion
+
+        #region GetAddInInfo
+        /// <summary>
+        /// This methods is invoked at the start of every test run and calls <see cref="IEdmAddIn5.GetAddInInfo(ref EdmAddInInfo, IEdmVault5, IEdmCmdMgr5)()"/>
+        /// </summary>
+        public override void Startup()
+        {
+            base.Startup();
+        }
+
+        #endregion
+
+        [PDMTestMethod()]
+        public void TestMethod()
+        {
+
+        }
+
+    }
+
+    #region placeholder add-in
+    public class T : IEdmAddIn5
+    {
+        public void GetAddInInfo(ref EdmAddInInfo poInfo, IEdmVault5 poVault, IEdmCmdMgr5 poCmdMgr)
+        {
+            throw new NotImplementedException("Use your own add-in class!");
+        }
+
+        public void OnCmd(ref EdmCmd poCmd, ref EdmCmdData[] ppoData)
+        {
+            throw new NotImplementedException("Use your own add-in class!");
+        }
+    }
+
+    #endregion
+}
