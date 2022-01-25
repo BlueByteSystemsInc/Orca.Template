43cf650cfd2ad49ec4af69969e5bd3ed9ba3d194
Andrey Arekhva <a.arekhva@gmail.com>
2022-01-25 19:08:19 +0300

    #1 Getting file vaults from the registry

--- Orca.Template.Wizard/ProjectDetails.cs
+++ Orca.Template.Wizard/ProjectDetails.cs
@@ -1,4 +1,8 @@
-﻿namespace Orca.Template.Wizard
+﻿using Microsoft.Win32;
+using System;
+using System.Linq;
+
+namespace Orca.Template.Wizard
 {
     public class ProjectDetails
     {
@@ -6,11 +10,34 @@
         public string LicensePath { get; set; }
         public string OrcaVersion { get; set; } = "latest";
 
+        public string[] FileVaults { get; }
+
         public ProjectDetails()
         {
             var settings = new WizardSettings();
             settings.Load();
             LicensePath = settings.LicencePath;
+
+            FileVaults = GetFileVaultsFromRegistry();
+        }
+
+        private string[] GetFileVaultsFromRegistry()
+        {
+            try
+            {
+                using var serversKey = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\SolidWorks\Applications\PDMWorks Enterprise\ConisioAdmin\Servers\");
+                var server = serversKey?.GetSubKeyNames().FirstOrDefault();
+                if (server != null)
+                {
+                    using var key = serversKey.OpenSubKey(server + @"\FileVaults");
+                    return key?.GetSubKeyNames();
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Error while getting registry values: " + ex.ToString());
+            }
+            return new string[0];
         }
     }
 }
